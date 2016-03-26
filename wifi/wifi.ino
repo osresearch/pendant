0a0e8f1c78555f0c90fdb947362771e6ee295acf
Trammell Hudson <hudson@trmm.net>
2016-03-26 15:53:49 -0400

    rearrange to work with arduino 1.6.7

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -289,6 +289,51 @@ void wifi_leader()
 	// nothing to do yet
 }
 
+// This is code from Adafruit
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return leds.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return leds.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return leds.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+int brightness = 0;
+int direction = 1;
+void scanning_pattern() {
+	for (int i = 0; i < NUM_PIXELS; i++) {
+		leds.setPixelColor(i, brightness, brightness, brightness);
+	}
+	leds.show();
+
+	if (brightness == 255) {
+		direction = -1;
+	} else if (brightness == 0) {
+		direction = 1;
+	}
+
+	brightness = brightness + direction;
+	
+}
+
+// This is code was adapted from code from Adafruit
+// Makes the rainbow equally distributed throughout
+void rainbowCycle() {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< leds.numPixels(); i++) {
+      leds.setPixelColor(i, Wheel(((i * 256 / leds.numPixels()) + j) & 255));
+    }
+    leds.show();
+  }
+}
+
 void loop()
 {
 	switch(wifi_mode)
@@ -401,48 +446,3 @@ void loop()
 */
 }
 
-int brightness = 0;
-int direction = 1;
-void scanning_pattern() {
-	for (int i = 0; i < NUM_PIXELS; i++) {
-		leds.setPixelColor(i, brightness, brightness, brightness);
-	}
-	leds.show();
-
-	if (brightness == 255) {
-		direction = -1;
-	} else if (brightness == 0) {
-		direction = 1;
-	}
-
-	brightness = brightness + direction;
-	
-}
-
-// This is code was adapted from code from Adafruit
-// Makes the rainbow equally distributed throughout
-void rainbowCycle() {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< leds.numPixels(); i++) {
-      leds.setPixelColor(i, Wheel(((i * 256 / leds.numPixels()) + j) & 255));
-    }
-    leds.show();
-  }
-}
-
-// This is code from Adafruit
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return leds.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return leds.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return leds.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
